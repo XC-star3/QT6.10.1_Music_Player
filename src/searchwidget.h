dd744a7005ec46811d7be2cb9354ab2c7271a36a
XC-star3 <209358371@qq.com>
2025-12-01 13:32:10 +0800

    初始版本

---
+++ src/searchwidget.h
@@ -0,0 +1,23 @@
+#ifndef SEARCHWIDGET_H
+#define SEARCHWIDGET_H
+
+#include "qboxlayout.h"
+#include <QWidget>
+#include <QTableWidget>
+#include <QHeaderView>
+
+class searchwidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit searchwidget(QWidget *parent = nullptr);
+    void displaySearchResults(const QStringList &results);
+
+private:
+    QTableWidget *tableWidget;
+
+Q_SIGNALS:
+    void songDoubleClicked(const QString &songName);
+};
+
+#endif // SEARCHWIDGET_H
